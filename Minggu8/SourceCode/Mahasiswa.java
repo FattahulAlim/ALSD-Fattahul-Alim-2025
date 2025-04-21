0c4e82d46cfc15e87b51f78a6d7e2ae04252e90d
FattahulAlim <fathalim1512@gmail.com>
2025-04-21 14:53:03 +0700

    Add Class mahasiswa and matakuliah

---
+++ Minggu8/SourceCode/Mahasiswa.java
@@ -0,0 +1,20 @@
+package Minggu8.SourceCode;
+
+public class Mahasiswa {
+    String nim;
+    String namaMahasiswa;
+
+    public Mahasiswa(){
+
+    }
+
+    public Mahasiswa(String nim, String nm){
+        this.nim = nim;
+        namaMahasiswa = nm;
+    }
+
+    void tampilData(){
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM:    " + nim + " |   Nama:   " + namaMahasiswa);
+    }
+}
